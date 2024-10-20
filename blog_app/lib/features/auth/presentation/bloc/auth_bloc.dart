d96229c82f5fcb1700fa554e5a03cb79b8c69aac
Bilelbourgou <bilelbourgou@gmail.com>
2024-10-20 14:22:11 +0100

    authentification completed

--- blog_app/lib/features/auth/presentation/bloc/auth_bloc.dart
+++ blog_app/lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,5 +1,4 @@
-
-
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -7,7 +6,21 @@ part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthSignUp>((event, emit) => null);
+  final UserSignUp _userSignUp;
+  AuthBloc({
+    required UserSignUp userSignup,
+  })  : _userSignUp = userSignup,
+        super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) async {
+      final res = await _userSignUp(UserSignUpParams(
+        email: event.email,
+        password: event.password,
+        name: event.name,
+      ));
+      res.fold(
+        (l) => emit(AuthFailure(message: l.meassage)),
+        (r) => emit(AuthSuccess(uid: r)),
+      );
+    });
   }
 }
